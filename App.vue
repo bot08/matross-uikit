ea4ab68d5638a6d0f4f6233d677e2579842ae49b
Oleg <scherbaoleg4@gmail.com>
2022-03-14 16:16:37 +0200

    add some components

--- App.vue
+++ App.vue
@@ -1,10 +1,30 @@
 <template>
   <main>
-    <Button/>
+    <!-- Theme toggle -->
+    <div class="bg-gray-50 dark:bg-gray-700 transition-colors w-full p-4 mb-2">
+      <ThemeToggle/>
+    </div>
+
+    <!-- Button on background -->
+    <div class="bg-gray-50 dark:bg-gray-700 transition-colors w-full p-4 my-2">
+      <Button/>
+    </div>
+    <!-- Without background -->
+    <Button class="m-4"/>
+
+    <!-- Loading button on background -->
+    <div class="bg-gray-50 dark:bg-gray-700 transition-colors w-full p-4 my-2">
+      <ButtonLoading/>
+    </div>
+    <!-- Without background -->
+    <ButtonLoading class="m-4"/>
+    
   </main>
 </template>
 
 
 <script setup>
+import ThemeToggle from './components/ThemeToggle.vue'
 import Button from './components/Button.vue'
+import ButtonLoading from './components/ButtonLoading.vue'
 </script>
---
+++ components/ThemeToggle.vue
@@ -0,0 +1,32 @@
+<template>
+    <div @click="themeSwitch()" class="dark:bg-gray-800 bg-gray-200
+                                         relative flex-shrink-0 cursor-pointer
+                                         h-[34px] w-[66px] 
+                                         border-2 border-transparent 
+                                         rounded-full 
+                                         transition-colors ease-in-out focus:outline-none">
+        <span class="sr-only">Theme switch</span>
+        <span class="dark:translate-x-8 translate-x-0 
+                     pointer-events-none inline-block ring-0
+                     h-[30px] w-[30px] 
+                     rounded-full bg-white dark:bg-gray-200 shadow-lg 
+                     transform transition ease-in-out"/>
+    </div>
+</template>
+
+
+<script setup>
+function themeSwitch() {
+    const html = document.querySelector("html");
+    if(localStorage.getItem('theme')=="dark"){
+        html.classList.remove("dark");
+        localStorage.setItem("theme", "white");
+        document.querySelector('meta[name="theme-color"]').setAttribute("content", "#f9fafb");
+    }
+    else{
+        html.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+        document.querySelector('meta[name="theme-color"]').setAttribute("content", "#374151");
+    }
+}
+</script>
